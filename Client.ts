12695345fc715381324b955e7dc4934a2c35c35b
ExternalAddress4401 <external_address@hotmail.com>
2025-10-28 12:27:18 -0700

    Further fix extractPackets

--- Client.ts
+++ Client.ts
@@ -21,10 +21,13 @@ export class Client {
   }
   extractPackets() {
     const packets = [];
-    const handler = new ProtobufHandler("READ", this.buffer);
-    while (handler.hasMore()) {
+    while (true) {
+      const handler = new ProtobufHandler("READ", this.buffer);
+      if (!handler.hasMore()) {
+        break;
+      }
       const length = handler.readIntBE() + 4;
-      if (handler.buffer.length - handler.index >= length) {
+      if (handler.buffer.length >= length) {
         handler.index -= 4;
         packets.push(new Packet(handler.slice(length)));
         this.buffer = this.buffer.subarray(length);
